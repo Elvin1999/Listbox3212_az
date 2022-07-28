dce8c7257c75651c79bf9991abbd4b4a95bf8cf7
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-07-28 10:49:14 +0400

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //List<string> cars = new List<string>
+            //{
+            //    "Audi",
+            //    "Mercedes",
+            //    "Chevrolet",
+            //    "Bugatti"
+            //};
+
+            //listBox1.DataSource = cars;
+            //listBox1.SelectedIndex = 2;
+            //listBox1.Items.Add("Maserati");
+            //listBox1.Items.Add("Wrangler");
+
+            List<Student> students = new List<Student>
+            {
+                new Student
+                {
+                     Name="John",
+                     Age=25
+                },
+                new Student
+                {
+                     Name="Leyla",
+                     Age=32
+                },
+                new Student
+                {
+                     Name="Tofiq",
+                     Age=42
+                },
+                new Student
+                {
+                     Name="Elikram",
+                     Age=76
+                },
+            };
+
+            listBox1.DisplayMember = nameof(Student.Name);
+            //listBox1.DataSource = students;
+            listBox1.Items.AddRange(students.ToArray());
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //var selectedItem=listBox1.SelectedItem as string;
+            //var selectedItem = listBox1.SelectedItem as Student;
+
+            //label1.Text = $"{selectedItem.Id} {selectedItem.Name} {selectedItem.Age}";
+            label1.Text = String.Empty;
+            var selectedItems = listBox1.SelectedItems;
+            foreach (var item in selectedItems)
+            {
+                var student = item as Student;
+                label1.Text += $"{student.Id} {student.Name} {student.Age}\n";
+            }
+
+            //MessageBox.Show(selectedItem);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource=null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = String.Empty;
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+    }
+}
---
+++ WindowsFormsApp2/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class Student
+    {
+        public string Id { get; set; }=Guid.NewGuid().ToString().Substring(0,8);
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Id}  {Name}";
+        }
+    }
+}
